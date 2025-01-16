8a115509e0f794e028e3b2ebb6111072aa357a64
Lander <lander.labaka@ikasle.aeg.eus>
2025-01-16 11:22:39 +0100

    [DEVELOP][ADD] Test 2

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,6 +1,6 @@
 import { potions } from "../data/data";
 import '@testing-library/jest-dom'
-import { filterByLevelRequireMent } from "../helpers/potionHelpers";
+import { filterByLevelRequireMent, getPotionsByRarity } from "../helpers/potionHelpers";
 
 // TEST 1
 describe('Recibe las pociones, el level y devuelve un array de pociones', () => {
@@ -18,6 +18,27 @@ describe('Recibe las pociones, el level y devuelve un array de pociones', () => 
 
         // Nivel 15 solo filrara una pocion
         const filteredPotions = filterByLevelRequireMent(potionsArray, 15);
+
+        //Assert
+        expect(filteredPotions.length).toBe(1);
+    
+    });
+});
+
+// TEST 2
+describe('Recibe las pociones, la rareza y devuelve un array de pociones', () => {
+    it('Devuelve todas las pociones que tienen una rareza especifica', () => {
+
+        // Arrange
+        const potion1 = potions[0];
+        const potion2 = potions[5];
+
+        let potionsArray = [potion1, potion2];
+
+        //Act
+
+        // Filtrara la pocion que sea rareza epic
+        const filteredPotions = getPotionsByRarity(potionsArray, 'epic');
 
         //Assert
         expect(filteredPotions.length).toBe(1);
