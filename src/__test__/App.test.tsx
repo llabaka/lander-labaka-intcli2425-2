1347c8df57adc826b1c4a7b5000e25d04ef60862
Lander <lander.labaka@ikasle.aeg.eus>
2025-01-16 11:19:17 +0100

    [DEVELOP][ADD] First test

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,9 +1,12 @@
 import { potions } from "../data/data";
 import '@testing-library/jest-dom'
+import { filterByLevelRequireMent } from "../helpers/potionHelpers";
 
+// TEST 1
 describe('Recibe las pociones, el level y devuelve un array de pociones', () => {
     it('Filtra las pociones que requieren un nivel de uso menor o igual al especificado', () => {
 
+        // Arrange
         const potion1 = potions[0];
         const potion2 = potions[1];
 
@@ -11,6 +14,13 @@ describe('Recibe las pociones, el level y devuelve un array de pociones', () => 
 
         console.log(potionsArray);
 
-        
+        //Act
+
+        // Nivel 15 solo filrara una pocion
+        const filteredPotions = filterByLevelRequireMent(potionsArray, 15);
+
+        //Assert
+        expect(filteredPotions.length).toBe(1);
+    
     });
 });
