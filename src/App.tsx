736e60f017c15bec33e6cd4534977b9448f294a0
llabaka <lander.labaka@ikasle.aeg.eus>
2025-01-22 17:21:30 +0100

    [MAIN][ADD] Potions component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Potion } from './types/Potion'
 import { potions } from './data/data'
 import { filterByLevelRequireMent } from './helpers/potionHelpers';
 import Modal from './components/Modal';
+import Potions from './components/Potions';
 
 function App() {
   const [potionsData, setPotionsData] = useState<Potion[] | []>([]);
@@ -23,28 +24,12 @@ function App() {
 
   return (
     <>
-      <div className="grid grid-cols-5 gap-4">
-        {potionsData.map((potion, index) => (
-          <div key={index} className="text-center p-4 rounded-lg">
-            <img src={potion.image} alt={`Potion ${potion.name}`} className="w-full h-auto mb-2" />
-            <p>{potion.name}</p>
-            <p>{potion.rarity}</p>
-            <p>{potion.meta.availability.drop_rate.boss}</p>
-            <p>{potion.meta.availability.drop_rate.chance}</p>
-            <button
-              className=" mt-2 bg-orange-500 hover:bg-orange-600 text-black font-bold py-2 px-4 border-b-4 border-orange-500 hover:border-orange-700 rounded "
-              onClick={() => openModal(potion)}
-            >
-              Show details
-            </button>
-          </div>
-        ))}
-      </div>
 
+      <Potions potionsData={potionsData} openModal={openModal} />
 
       {potionModalVisible && selectedPotion && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-          <Modal potion={selectedPotion} setPotionModalVisible={setPotionModalVisible}/>
+          <Modal potion={selectedPotion} setPotionModalVisible={setPotionModalVisible} />
         </div>
       )}
     </>
---
+++ src/components/Potions.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Potion } from "../types/Potion";
+
+
+
+interface PotionsProps {
+    potionsData: Potion[];
+    openModal: (potion: Potion) => void;
+}
+
+
+const Potions: React.FC<PotionsProps> = ({ potionsData, openModal }) => {
+    return (
+        <div className="grid grid-cols-5 gap-4">
+            {potionsData.map((potion, index) => (
+                <div key={index} className="text-center p-4 rounded-lg">
+                    <img src={potion.image} alt={`Potion ${potion.name}`} className="w-full h-auto mb-2" />
+                    <p>{potion.name}</p>
+                    <p>{potion.rarity}</p>
+                    <p>{potion.meta.availability.drop_rate.boss}</p>
+                    <p>{potion.meta.availability.drop_rate.chance}</p>
+                    <button
+                        className="mt-2 bg-orange-500 hover:bg-orange-600 text-black font-bold py-2 px-4 border-b-4 border-orange-500 hover:border-orange-700 rounded"
+                        onClick={() => openModal(potion)}
+                    >
+                        Show details
+                    </button>
+                </div>
+            ))}
+
+        </div>
+    )
+};
+
+export default Potions;
